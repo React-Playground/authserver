8f32a39bbba776c8b82c8f05671cbc2e8885b5c2
darwin <oleg.umarov@gmail.com>
2016-04-02 17:18:02 -0500

    create protected route

--- router.js
+++ router.js
@@ -1,4 +1,12 @@
+var passportService = require('./services/passport.js');
+var passport = require('passport');
 var auth = require('./controllers/auth.js');
+
+const requireAuth = passport.authenticate('jwt', {session: false});
+
 module.exports = function(app) {
+  app.get('/', requireAuth, function(req, res) {
+    res.send({hi: 'there'});
+  })
   app.post('/signup', auth.signup);
 }
